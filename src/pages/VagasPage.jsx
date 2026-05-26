b3c88c9e057c52cb75bf2325389816fa55085b26
Miguel Vitor Oliveira Vieira <miguelvitor20070@gmail.com>
2026-05-26 19:11:05 -0300

    feat: busca dados de vagas e renderiza lista e contador

--- src/pages/VagasPage.jsx
+++ src/pages/VagasPage.jsx
@@ -1,7 +1,19 @@
+import { useGetData } from "../hooks/useGetData"
+import { BASE_URL } from "../constants/urls"
+
 export function VagasPage() {
+
+    const [vagas, loading, error] = useGetData(`${BASE_URL}/vagas`)
+
+    const vagasList = vagas && vagas.body.vagas.map((vaga) => {
+        return <li key={vaga.id}>{vaga.cargo}</li>
+    })
     return (
         <>
             <h1>Vagas</h1>
+            <ul>
+                {vagasList}
+            </ul>
         </>
     )
 }
--- src/pages/admin/DashboardPage.jsx
+++ src/pages/admin/DashboardPage.jsx
@@ -5,15 +5,18 @@ export function DashboardPage() {
 
     const [dataNoticias, loadingNoticias, errorNoticias] = useGetData(`${BASE_URL}/noticias?recentes=900`)
     const [dataEventos, loadingEventos, errorEventos] = useGetData(`${BASE_URL}/eventos`)
+    const [dataVagas, loadingVagas, errorVagas] = useGetData(`${BASE_URL}/vagas`)
 
     const noticiasCount = dataNoticias && dataNoticias.body.noticias.length
     const eventosCount = dataEventos && dataEventos.body.eventos.length
+    const vagasCount = dataVagas && dataVagas.body.vagas.length
 
     return (
         <>
             <h1>Dashboard</h1>
             <p>Total de Notícias: {noticiasCount}</p>
             <p>Total de Eventos: {eventosCount}</p>
+            <p>Total de Vagas: {vagasCount}</p>
         </>
     );
 }
