3905acdcaaf2cb65dc3717d504e94b60e0551e19
Miguel Vitor Oliveira Vieira <miguelvitor20070@gmail.com>
2026-05-26 04:30:02 -0300

    feat: renderiza lista de eventos no EventosPage

--- src/pages/EventosPage.jsx
+++ src/pages/EventosPage.jsx
@@ -1,7 +1,20 @@
+import { useGetData } from "../hooks/useGetData"
+import { BASE_URL } from "../constants/urls"
+
 export function EventosPage() {
+
+    const [eventos, loading, error] = useGetData(`${BASE_URL}/eventos`)
+
+    const eventosList = eventos && eventos.body.eventos.map((evento) => {
+        return <li key={evento.id}>{evento.titulo}</li>
+    })
+    
     return (
         <>
             <h1>Eventos</h1>
+            <ul>
+                {eventosList}
+            </ul>
         </>
     );
 }
