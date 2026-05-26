7b3f0d7f29b3c5e4eb9a055aa3be669805266c54
Miguel Vitor Oliveira Vieira <miguelvitor20070@gmail.com>
2026-05-26 04:28:18 -0300

    feat: implementa lista de noticias

--- src/pages/NoticiasPage.jsx
+++ src/pages/NoticiasPage.jsx
@@ -1,7 +1,19 @@
+import { useGetData } from "../hooks/useGetData"
+import { BASE_URL } from "../constants/urls"
+
 export function NoticiasPage() {
+
+    const [noticias, loading, error] = useGetData(`${BASE_URL}/noticias?recentes=900`)
+
+    const noticiasList = noticias && noticias.body.noticias.map((noticia) => {
+        return <li key={noticia.id}>{noticia.titulo}</li>
+    })
     return (
         <>
             <h1>Notícias</h1>
+            <ul>
+                {noticiasList}
+            </ul>
         </>
     )
 }
