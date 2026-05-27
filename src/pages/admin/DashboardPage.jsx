44c571e01bc081ccfaab3272486a1617716afa41
Miguel Vitor Oliveira Vieira <miguelvitor20070@gmail.com>
2026-05-26 21:50:26 -0300

    feat: realiza contagem de exames no admin

--- src/pages/admin/DashboardPage.jsx
+++ src/pages/admin/DashboardPage.jsx
@@ -6,10 +6,20 @@ export function DashboardPage() {
     const [dataNoticias, loadingNoticias, errorNoticias] = useGetData(`${BASE_URL}/noticias?recentes=900`)
     const [dataEventos, loadingEventos, errorEventos] = useGetData(`${BASE_URL}/eventos`)
     const [dataVagas, loadingVagas, errorVagas] = useGetData(`${BASE_URL}/vagas`)
+    const [dataExames, loadingExames, errorExames] = useGetData(`${BASE_URL}/localizacao/overview`)
 
     const noticiasCount = dataNoticias && dataNoticias.body.noticias.length
     const eventosCount = dataEventos && dataEventos.body.eventos.length
     const vagasCount = dataVagas && dataVagas.body.vagas.length
+    let examesCount = 0
+
+    if (dataExames){
+        for (const bloco of dataExames.body.blocos) {
+            for (const setor of bloco.setores){
+                examesCount += setor.exames.length
+            }
+        }
+    }
 
     return (
         <>
@@ -17,6 +27,7 @@ export function DashboardPage() {
             <p>Total de Notícias: {noticiasCount}</p>
             <p>Total de Eventos: {eventosCount}</p>
             <p>Total de Vagas: {vagasCount}</p>
+            <p>Total de Exames: {examesCount}</p>
         </>
     );
 }
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route} from "react-router";
+import { Routes, Route } from "react-router";
 
 import { HomePage } from "../pages/HomePage";
 import { NoticiasPage } from "../pages/NoticiasPage";
@@ -9,8 +9,8 @@ import { LocalizacaoPage } from "../pages/LocalizacaoPage";
 import { LoginPage } from "../pages/admin/LoginPage";
 import { DashboardPage } from "../pages/admin/DashboardPage";
 
-function AppRoutes(){
-    return(
+function AppRoutes() {
+    return (
         <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/noticias" element={<NoticiasPage />} />
