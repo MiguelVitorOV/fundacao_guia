95c51bb8753e8e8c8cd81b95e20ec37c9d2ccce5
Breno Moreira soares <bnmoreirasoares@gmail.com>
2026-06-02 19:21:42 +0000

    feat: adiciona paleta de cores no tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,35 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        text: '#252B37',
+        primary: '#0C3B70',
+        secondary: '#DE1B62',
+        black: '#151A23',
+        white: '#FFFFFF',
+        blue: {
+          200: '#A1ABC4',
+          500: '#0C3B70',
+          800: '#111B2E',
+        },
+        pink: {
+          200: '#FBB1BD',
+          500: '#DE1B62',
+          800: '#551A29',
+        },
+        'blue-light': {
+          200: '#77D9F7',
+          500: '#23A6DE',
+          800: '#00355E',
+        },
+        neutral: {
+          200: '#D7DCE0',
+          500: '#646E82',
+          800: '#252B37',
+        },
+      },
+    },
   },
   plugins: [],
 }
